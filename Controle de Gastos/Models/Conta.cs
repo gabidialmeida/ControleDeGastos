87bba5b2381833388f707e61dca6ba678c311019
Gabriela Almeida De Souza <1077334@sga.pucminas.br>
2019-10-25 21:02:21 -0200

    Models

--- Controle de Gastos/Models/Conta.cs
+++ Controle de Gastos/Models/Conta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace ControleDeGastos.Models
 {
     public class Conta
     {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Saldo { get; set; }
     }
 }
